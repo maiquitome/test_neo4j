58fc160d163c097af2f7b09777848cc185411232
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-05-30 08:55:55 -0300

    fix: :wrench: change password

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,5 @@ import Config
 
 config :bolt_sips, Bolt,
   url: "bolt://localhost:7687",
-  basic_auth: [username: "neo4j", password: "admin"]
+  basic_auth: [username: "neo4j", password: "admin"],
+  pool_size: 10
